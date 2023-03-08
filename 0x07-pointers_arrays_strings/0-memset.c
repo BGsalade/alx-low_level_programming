020d616a04a9026b2b14483afe32543804a414c2
BGsalade <brandongabrielsalade@gmail.com>
2023-03-08 05:01:46 -0800

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,11 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-		int i = 0;
-
-		for (i = 0; i < n; i++)
-		{
+	int i = 0;
+	for (; n > 0; i++)
+	{
 		s[i] = b;
 		n--;
-		}
-                return (s);
+	}
+	return (s);
 }
