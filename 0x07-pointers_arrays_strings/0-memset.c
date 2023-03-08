6dca39d097741fb955a2eeeb6b2500a60eb88b2f
BGsalade <brandongabrielsalade@gmail.com>
2023-03-08 04:37:56 -0800

    0-memsett

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  *  * *_memset - fills memory with a constant byte
@@ -11,12 +10,12 @@
  *        */
 char *_memset(char *s, char b, unsigned int n)
 {
-		unsigned int i;
+		int i = 0;
 
-			for (i = 0; i < n; i++)
-			 {
-				s[i] = b;
-			 }
-
-				return (s);
+		for (i = 0; i < n; i++)
+		{
+		s[i] = b;
+		n--;
+		}
+                return (s);
 }
